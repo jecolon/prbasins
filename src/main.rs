8531750fa62416aada8fa2605b6bd176a9ced045
Jose Colon Rodriguez <jec.rod@gmail.com>
2022-08-05 19:40:30 -0400

    Update doc comment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 //! prbasins displays Puerto Rico water basin levels and status.
 //
 // Rust version of Edwood Ocasio's https://github.com/eocasio/embalses
-// Inspired by http://mate.uprh.edu/embalsespr/
 // Copyright 2022 Jose E. Colon <jec.rod@gmail.com> (a.k.a. Dude the Builder)
 //
 // Data provided by the CienciaDatosPR group of the University of Puerto Rico
